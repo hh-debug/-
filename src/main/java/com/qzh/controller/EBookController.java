df4710b7f617f70e4dc6b35ad89a769d047e00f3
itcast <qzh064425@163.com>
2021-07-09 14:18:09 +0800

    2-0 增加通用的返回类commonResp 以后后端接口所有的返回值都是此类型

--- src/main/java/com/qzh/controller/EBookController.java
+++ src/main/java/com/qzh/controller/EBookController.java
@@ -1,10 +1,10 @@
 package com.qzh.controller;
 
 import com.qzh.domain.Ebook;
+import com.qzh.resp.CommonResp;
 import com.qzh.service.EBookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,18 +16,27 @@ import java.util.List;
  * @date:2021/7/910:38
  */
 @RestController
-public class TestController {
-
-//    测试自定义配置 变量要加spring表达式
-    @Value("${hello.test}")
-    private String ConfigTestHello;
+@RequestMapping("/ebook")
+public class EBookController {
 
     //测试mybatis集成自动生成插件
     @Autowired
     private EBookServiceImpl eBookService;
 
-    @RequestMapping("/test")
-    public List<Ebook> hello(){
-        return eBookService.list();
+//    @RequestMapping("/list")
+//    public List<Ebook> hello(){
+//        return eBookService.list();
+//    }
+
+//    规范后端开发 为了前端能够统一处理逻辑 需要后端统一的返回值
+
+    @RequestMapping("/list")
+    public CommonResp list(){
+        CommonResp<List<Ebook>> commonResp = new CommonResp<>();
+        List<Ebook> list = eBookService.list();
+        commonResp.setContent(list);
+
+        return commonResp;
     }
+
 }
