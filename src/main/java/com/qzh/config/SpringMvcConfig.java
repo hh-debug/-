df5d70a7ca9473a88d1faf7055215e537c571b7b
itcast <qzh064425@163.com>
2021-07-10 18:08:18 +0800

    3-6 增加拦截器 打印日志

---
+++ src/main/java/com/qzh/config/SpringMvcConfig.java
@@ -0,0 +1,26 @@
+package com.qzh.config;
+
+import com.qzh.interceptor.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @packageName:com.qzh.config
+ * @ClassName:SpringMvcConfig
+ * @date:2021/7/1018:04
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+}
