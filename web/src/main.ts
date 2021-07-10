b3747400afd1fa519ac6d05989bb7fd47015ce0c
itcast <qzh064425@163.com>
2021-07-10 17:45:49 +0800

    3-4 配置axios拦截器

--- web/src/main.ts
+++ web/src/main.ts
@@ -12,3 +12,21 @@ Axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
 console.log('环境:',process.env.Node_ENV)
 console.log('服务端:',process.env.VUE_APP_SERVER)
+
+
+/**
+ * axios拦截器
+ */
+Axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+Axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
