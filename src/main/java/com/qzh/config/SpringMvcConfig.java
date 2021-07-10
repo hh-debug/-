c14a74a4ec2bf91c17cf6e359cb3c90106165fac
itcast <qzh064425@163.com>
2021-07-10 18:23:52 +0800

    3-7 增加aop 去掉过滤器跟拦截器

--- src/main/java/com/qzh/config/SpringMvcConfig.java
+++ src/main/java/com/qzh/config/SpringMvcConfig.java
@@ -1,7 +1,5 @@
 package com.qzh.config;
 
-import com.qzh.interceptor.LogInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,13 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private LogInterceptor logInterceptor;
+//    @Autowired
+//    private LogInterceptor logInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login");
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/login");
     }
 }
