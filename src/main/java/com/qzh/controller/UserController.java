b734c38380328d2956fdec0babd593450512c5c4
itcast <qzh064425@163.com>
2021-07-14 11:40:15 +0800

    9-1 集成redis 登录成功后放入redis中

--- src/main/java/com/qzh/controller/UserController.java
+++ src/main/java/com/qzh/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.qzh.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.qzh.req.UserLoginReq;
 import com.qzh.req.UserQueryReq;
 import com.qzh.req.UserResetPasswordReq;
@@ -9,11 +10,16 @@ import com.qzh.resp.PageResp;
 import com.qzh.resp.UserLoginResp;
 import com.qzh.resp.UserQueryResp;
 import com.qzh.service.UserService;
+import com.qzh.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @packageName:com.qzh.controller
@@ -24,8 +30,16 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Resource
+    private SnowFlake snowFlake;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -65,6 +79,14 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token:{},并放入redis中");
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token.toString(), JSONObject.toJSONString(userLoginResp),3600*24, TimeUnit.SECONDS);
+
+
+
         resp.setContent(userLoginResp);
         return resp;
     }
