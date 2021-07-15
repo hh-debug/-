77a5ebc51c0c690629f9a13ed1e00b776ccc2438
itcast <qzh064425@163.com>
2021-07-15 13:03:23 +0800

    10-1 定时任务示例

--- src/main/java/com/qzh/WikiApplication.java
+++ src/main/java/com/qzh/WikiApplication.java
@@ -6,10 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @MapperScan("com.qzh.mapper")
+@EnableScheduling
 public class WikiApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
