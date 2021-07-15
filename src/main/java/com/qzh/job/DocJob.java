2cb5c5c1b9f916ae29be00d7605a2c0fcd5f59fe
itcast <qzh064425@163.com>
2021-07-15 16:16:10 +0800

    10-4 增加日志流水号

--- src/main/java/com/qzh/job/DocJob.java
+++ src/main/java/com/qzh/job/DocJob.java
@@ -2,6 +2,8 @@ package com.qzh.job;
 
 
 import com.qzh.service.impl.DocServiceImpl;
+import com.qzh.util.SnowFlake;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +15,16 @@ public class DocJob {
     @Resource
     private DocServiceImpl docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
     /**
      * 每30秒更新电子书信息
      */
     @Scheduled(cron = "1/5 * * * * ?")
     public void cron() {
+        // 增加日志流水号
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         docService.updateEbookInfo();
     }
 
